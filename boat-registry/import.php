c7d9ef702b7fa6d756fcfc3bc71826eff174bf76
Marcin Pietrzak <marcin@iworks.pl>
2020-06-24 16:13:45 +0200

    import

--- boat-registry/import.php
+++ boat-registry/import.php
@@ -23,7 +23,7 @@ $_SERVER['HTTP_HOST'] = 'int505';
 
 error_reporting( E_ALL );
 
-require '../wp-load.php';
+require '/var/virtuals/wordpress/wp-load.php';
 require 'functions.php';
 
 global $wpdb;
@@ -41,33 +41,6 @@ foreach ( $data as $one ) {
 }
 
 $persons = array();
-
-function get_person_by_name( $post_title ) {
-	global $persons, $person_post_type_name;
-	if ( 5 > strlen( $post_title ) ) {
-		return null;
-	}
-	$post_title = person_clear_name( $post_title );
-	if ( isset( $persons[ $post_title ] ) ) {
-		return $persons[ $post_title ];
-	}
-	$person = get_page_by_title( $post_title, OBJECT, $person_post_type_name );
-	if ( empty( $person ) ) {
-		$args = array(
-			'post_status' => 'publish',
-			'post_type'   => $person_post_type_name,
-			'post_title'  => $post_title,
-
-		);
-		$post_ID                = wp_insert_post( $args );
-		$persons[ $post_title ] = get_post( $post_ID, OBJECT );
-	} else {
-		$persons[ $post_title ] = $person;
-	}
-	return $persons[ $post_title ];
-}
-
-
 
 $rows = array();
 if ( ( $handle = fopen( 'registry.csv', 'r' ) ) !== false ) {
@@ -314,78 +287,10 @@ if ( ( $handle = fopen( 'registry.csv', 'r' ) ) !== false ) {
 	);
 
 	$update_terms = get_terms( $get_terms_args );
-	wp_update_term_count_now( $update_terms, $update_taxonomy );
+	print_r( $update_terms );
 
-
+	wp_update_term_count_now( $update_terms, $taxonomy_name_manufacturer );
 
 	fclose( $handle );
-}
-
-function person_clear_name( $name ) {
-	$name      = preg_replace( '/ \' ?\d+$/', '', $name );
-	$is_person = check_is_person( $name );
-	if ( $is_person ) {
-		$name = preg_replace( '/ [A-Z]{2,3}$/', '', $name );
-	}
-	switch ( $name ) {
-		case 'St. Vincents Gulf 505 Association AUS':
-			return 'St. Vincents Gulf 505 Association';
-		case 'Tom Bojland':
-			return 'Tom Bøjland';
-		case 'Wolfgang Stuckl':
-		case 'Wolfgang Stueckl':
-		case 'Wolfgang Stuekl':
-			return 'Wolfgang Stückl';
-		case 'Y. Pajot':
-			return 'Yves Pajot';
-	}
-	return $name;
-}
-
-
-
-function person( $raw, $person, $date_from = '', $order = false ) {
-	if ( empty( $date_from ) && preg_match( '/ \\d{2}$/', $raw, $matches ) ) {
-		$year = intval( $matches[1] );
-		if ( 54 < $year ) {
-			$year += 100;
-		}
-		$year     += 1900;
-		$date_from = sprintf( '%d-01-01', $year );
-	}
-	return wp_parse_args(
-		array(
-			'first'     => 'first' === $order,
-			'current'   => 'current' === $order,
-			'user_id'   => $person->ID,
-			'date_from' => $date_from,
-		),
-		array(
-			'current'   => false,
-			'first'     => false,
-			'users_ids' => array(),
-			'date_from' => '',
-			'date_to'   => '',
-		)
-	);
-}
-
-
-function check_is_person( $data ) {
-	switch ( $data ) {
-		case 'St. George\'s School Sailing Club':
-		case 'St. Johns Jr. College':
-		case 'St. Vincents Gulf 505 Association':
-		case 'Sawanaka Corinthian Yacht Club':
-		case 'Burnham-Sharpe Co.':
-		case 'Sailing club in Washington State':
-		case 'School in Queen Anne':
-		case 'some New England sailing academy.':
-		case 'US Coastguard Academy':
-		case 'Wansborough family':
-		case 'Web Institute':
-			return false;
-	}
-	return true;
 }
 
